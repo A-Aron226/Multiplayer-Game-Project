3dde187a8559f2fc8636e5e6de579aff5a3a6fb8
A-Aron226 <aroeder345@gmail.com>
2024-10-22 12:16:57 -0500

    Moved scripts and cleaned up scene

---
+++ Assets/Scipts/Enemy Scripts/Enemy.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public enum EnemyStates
+{
+    Patrol,
+    Pursue
+}
+public class Enemy : MonoBehaviour
+{
+    //For patrol functions
+    //Accessible in Script
+    NavMeshAgent agent;
+    public Transform[] waypoints;
+    int waypointIndex;
+    Vector3 target;
+    FieldOfView view;
+
+
+    //For enemy attacking
+    //Accessible in Editor
+    public float attackDamage;
+    public float speed;
+    [SerializeField] Transform playerTarget;
+
+
+    //Accessible in Script
+    Rigidbody rb;
+    NavMeshPath path;
+    GameObject player;
+    EnemyStates state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        view = GetComponent<FieldOfView>();
+        agent = GetComponent<NavMeshAgent>();
+        rb = GetComponent<Rigidbody>();
+        path = new NavMeshPath();
+
+        agent.CalculatePath(playerTarget.position, path);
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        UpdateDestination();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!view.canSeePlayer)
+        {
+            state = EnemyStates.Patrol;
+        }
+
+        else
+        {
+            state = EnemyStates.Pursue;
+        }
+
+        switch (state)
+        {
+            case EnemyStates.Patrol:
+                UpdatePatrol();
+                break;
+            case EnemyStates.Pursue:
+                UpdatePursue();
+                break;
+        }
+    }
+
+    //Functions for the patrol state
+    public void UpdatePatrol()
+    {
+        if (Vector3.Distance(transform.position, target) < 1)
+        {
+            IterateWaypointIndex();
+            UpdateDestination();
+        }
+    }
+
+    void UpdateDestination()
+    {
+        target = waypoints[waypointIndex].position;
+        agent.SetDestination(target);
+    }
+
+    void IterateWaypointIndex()
+    {
+        waypointIndex++;
+        if (waypointIndex == waypoints.Length) //When AI reaches to last waypoint, it will go back to the first point
+        {
+            waypointIndex = 0;
+        }
+    }
+
+    public void UpdatePursue()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, playerTarget.transform.position, speed * Time.fixedDeltaTime);
+    }
+}
