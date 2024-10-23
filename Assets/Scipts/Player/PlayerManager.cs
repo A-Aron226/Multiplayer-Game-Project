930291c04ae49a3f4b9acc848fd4efa8780299bd
hoodey <41493055+hoodey@users.noreply.github.com>
2024-10-23 14:12:57 -0500

    Got cameras to actually go to correct player
    Each camera now only views from the correct player instead of all of them looking at one player.

--- Assets/Scipts/Player/PlayerManager.cs
+++ Assets/Scipts/Player/PlayerManager.cs
@@ -29,25 +29,23 @@ public class PlayerManager : MonoBehaviour
 
     public void AddPlayer(PlayerInput player)
     {
+        //add this playerinput for our player object to the list of playerinputs
         players.Add(player);
-
+        //obtain the parent of this object as the player has an empty parent with several children
         Transform playerParent = player.transform.parent;
 
         //convert layer mask (bit) to an integer
         int layerToAdd = (int)Mathf.Log(playerLayers[players.Count - 1].value, 2);
 
+
         //set the layer
         playerParent.GetComponentInChildren<CinemachineVirtualCamera>().gameObject.layer = layerToAdd;
+        //get value of current player's layer so we can add it to our culling mask
+        string playerCurrentLayer = LayerMask.LayerToName(layerToAdd);
         //add the layer
-        Debug.Log(playerParent.GetComponentInChildren<Camera>().cullingMask);
-        playerParent.GetComponentInChildren<Camera>().cullingMask |= 1 << layerToAdd;
-        Debug.Log(playerParent.GetComponentInChildren<Camera>().cullingMask);
+        playerParent.GetComponentInChildren<Camera>().cullingMask = LayerMask.GetMask("Default", "TransparentFX", "Ignore Raycast", "Water", "UI", "Rig", playerCurrentLayer);
         //set the player index in the cinemachine Input Handler
         playerParent.GetComponentInChildren<CinemachineInputProvider>().PlayerIndex = player.playerIndex;
-
-        //Check if player is using keyboard, then assign the mouse to them
-        //if (player.currentControlScheme == "KeyboardMouse")
-        Debug.Log(player.currentControlScheme);
 
     }
 }
