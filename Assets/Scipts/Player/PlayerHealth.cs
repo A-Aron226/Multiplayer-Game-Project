4bf9f132e8a5e2c024ead457b6776693b2051cd1
A-Aron226 <aroeder345@gmail.com>
2024-10-28 13:31:12 -0500

    Health HUD created

--- Assets/Scipts/Player/PlayerHealth.cs
+++ Assets/Scipts/Player/PlayerHealth.cs
@@ -2,13 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] HealthSO health;
+    [SerializeField] Image healthBar;
     // Start is called before the first frame update
     void Start()
     {
         health.currHealth = health.maxHealth;
+    }
+
+    void Update()
+    {
+        healthBar.fillAmount = health.currHealth / health.maxHealth;
     }
 }
