1b576f2ae928eb85172d0b3171aae556d184dd5c
A-Aron226 <aroeder345@gmail.com>
2024-10-29 14:25:32 -0500

    Added director setup; will need fixing

--- Assets/Scipts/ItemScripts/HealthSpawnDirector.cs
+++ Assets/Scipts/ItemScripts/HealthSpawnDirector.cs
@@ -8,7 +8,23 @@ public class HealthSpawnDirector : MonoBehaviour
     [SerializeField] HealthSO health;
     [SerializeField] Transform[] spawnPoints;
     [SerializeField] GameObject healthPack;
-    [SerializeField] float time;
+    public float timer;
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= 25)
+        {
+            timer = 0;
+        }
+
+        if (health.currHealth < 50f && timer >= 15 && timer <= 20) //Spawns health pack if the player is less than 50% health within the timer range
+        {
+            Spawn();
+            Debug.Log("Health Pack Spawned");
+        }
+    }
 
     void Spawn() //Will spawn a health pack item from pool
     {
